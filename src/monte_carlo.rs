50b3d3607ee9382029a55f39a02aa00f9ccae109
Felix Jablinski <jablinskifelix@gmail.com>
2024-03-26 11:23:35 +0100

    Move board-generator to simulation

--- src/monte_carlo.rs
+++ src/monte_carlo.rs
@@ -1,4 +1,5 @@
-use battleship::board::{Board, Ship};
+use battleship::board::{Board, Direction, Ship};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 pub struct MonteCarlo {
     pub n: usize,
@@ -10,10 +11,10 @@ impl MonteCarlo {
         let mut results = vec![0usize; Board::WIDTH * Board::HEIGHT];
 
         for _ in 0..self.n {
-            let mut board = Board::random(&self.ships);
+            let mut board = MonteCarlo::random(&self.ships);
 
             for result in &mut results {
-                let max_index = board.get_max_density_index().unwrap();
+                let max_index = board.max_density_index().unwrap();
                 board.shoot(max_index % Board::WIDTH, max_index / Board::WIDTH);
 
                 if board.is_over() {
@@ -32,5 +33,62 @@ impl MonteCarlo {
         }
 
         println!("{:?}", results);
+    }
+
+    pub fn random(ships: &Vec<Ship>) -> Board {
+        let mut board = Board::default();
+
+        let mut rand = Rand::default();
+        for ship in ships {
+            loop {
+                let (x, y) = (
+                    (rand.random() * Board::WIDTH as f64) as usize,
+                    (rand.random() * Board::HEIGHT as f64) as usize,
+                );
+
+                let direction = match rand.random_raw() & 1 {
+                    0 => Direction::Horizontal,
+                    _ => Direction::Vertical,
+                };
+
+                if board.place(*ship, x, y, direction) {
+                    break;
+                }
+            }
+        }
+
+        board
+    }
+}
+
+struct Rand {
+    pub seed: u128,
+    multiplier: u128,
+}
+
+impl Default for Rand {
+    fn default() -> Self {
+        let nanos = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap_or_default()
+            .as_nanos();
+
+        Self {
+            seed: nanos | 1,
+            multiplier: 0xF1C47040DE494ACC251D055F00F0A1AB,
+        }
+    }
+}
+
+impl Rand {
+    pub fn random(&mut self) -> f64 {
+        self.random_raw() as f64 / u64::MAX as f64
+    }
+
+    pub fn random_raw(&mut self) -> u64 {
+        self.seed = self.seed.wrapping_mul(self.multiplier);
+        let rot = (self.seed >> 122) as u32;
+        let xsl = (self.seed >> 64) as u64 ^ self.seed as u64;
+        xsl.rotate_right(rot)
     }
 }
